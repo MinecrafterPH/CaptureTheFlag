93ed73f9d310a2817e6ea4c027725629091027f8
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-05-28 17:17:23 +0800

    Updated messages

--- src/ctf/timer.php
+++ src/ctf/timer.php
@@ -21,7 +21,7 @@ class timer extends PluginTask{
                 $times = $this->times + 1;
                 $this->times = $times;
                 $remaining = $this->getOwner()->getConfig()->get("Game Length (Mins)") - $this->times;
-                $this->getServer()->broadcastMessage(TextFormat::RED . round($remaining) . " minutes remain" );
+                $this->getServer()->broadcastMessage(TextFormat::RED . round($remaining) . " minutes remaining." );
                 
             }
             
@@ -44,8 +44,9 @@ class timer extends PluginTask{
             $red = 1;
         }
        //FIX DIS $this->getServer()->broadcastMessage("Somebody scored a point!");
-        $this->getServer()->broadcastMessage(TextFormat::DARK_PURPLE . "Rebels: ". $red);
-        $this->getServer()->broadcastMessage(TextFormat::GREEN . "The Kool Kids: ". $blue);
+        $this->getServer()->broadcastMessage("Scores:");
+        $this->getServer()->broadcastMessage(TextFormat::DARK_PURPLE . "Red: ". $red);
+        $this->getServer()->broadcastMessage(TextFormat::GREEN . "Blue: ". $blue);
             $red = $this->getOwner()->temp["RedPoints"];
             $blue = $this->getOwner()->temp["BluePoints"];
             
@@ -58,8 +59,8 @@ class timer extends PluginTask{
             
             if ($this->getOwner()->temp["BluePoints"] > $this->getOwner()->temp["RedPoints"]) {
                 $this->broadcastScores();
-                $this->getServer()->broadcastMessage("THE BLUE TEAM WINS!");
-                $this->getServer()->broadcastMessage("Server will now restart");
+                $this->getServer()->broadcastMessage("The Blue team wins the game!");
+                $this->getServer()->broadcastMessage("Server will now restart.");
                 $this->getServer()->broadcastMessage("Please rejoin to play again!");
                 sleep(3);
                 $this->getServer()->shutdown();
@@ -67,14 +68,15 @@ class timer extends PluginTask{
             else if ($this->getOwner()->temp["BluePoints"] < $this->getOwner()->temp["RedPoints"])
                 {
                 $this->broadcastScores();
-                $this->getServer()->broadcastMessage("THE RED TEAM WINS!");
+                $this->getServer()->broadcastMessage("The Red team wins the game!");
                 $this->getServer()->broadcastMessage("Server will now restart");
                 $this->getServer()->broadcastMessage("Please rejoin to play again!");
                 sleep(3);
                 $this->getServer()->shutdown();
             }
             else {
-                $this->getServer()->broadcastMessage("The game ended in a tie!");
+                $this->getServer()->broadcastMessage("The game ended with a tie!");
+                $this->getServer()->broadcastMessage("Server will now restart");        
                 $this->getServer()->broadcastMessage("Please rejoin to play again!");
                 sleep(3);
                 $this->getServer()->shutdown();
